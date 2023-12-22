09c3e8c055230a0c1fbb83084a19fddecfa0a0b6
NathanWeiland10 <77362517+NathanWeiland10@users.noreply.github.com>
2023-12-22 18:12:21 -0500

    Updated Project Icons

--- src/app/project-list.ts
+++ src/app/project-list.ts
@@ -6,28 +6,28 @@ export const PROJECT_LIST: Project[] = [
     description: "Began work during the Summer of 2020 just before taking classes at Wright State University. Upgrade your weapons, character, and explore a zombie-infested abandoned city in this top down shooter!",
     projectType: "C# (Unity)",
     gitHubLink: "https://github.com/NathanWeiland10/ZombieShooter-Unity/tree/main",
-    projectCover: "pc-icon.png",
+    projectCover: "ZombieShooterIcon.png",
   },
   {
     title: "Java Quest",
     description: "Completed during my first year of college in the Spring of 2021 and was the winner of a game jam that was held against five other student projects. Encounter various types of enemies and level up your character in this text based adventure game!",
     projectType: "Java (Java Swing)",
     gitHubLink: "https://github.com/NathanWeiland10/JavaQuest/tree/main",
-    projectCover: "pc-icon.png",
+    projectCover: "JavaQuestIcon.png",
   },
   {
     title: "Bowling Game",
     description: "Completed during the Summer of 2021 through a game programming course offered at Wright State. Strike out in this interactive bowling game!",
     projectType: "C# (Unity)",
     gitHubLink: "https://github.com/NathanWeiland10/BowlingGame-Unity",
-    projectCover: "pc-icon.png",
+    projectCover: "BowlingGameIcon.png",
   },
   {
     title: "Tiny Tanks",
     description: "Completed during the Summer of 2021 through a game programming course offered at Wright State. Upgrade your tank and fight off against an endless wave of enemy planes!",
     projectType: "C# (Unity)",
     gitHubLink: "https://github.com/NathanWeiland10/TinyTanks-Unity/tree/main",
-    projectCover: "pc-icon.png",
+    projectCover: "TinyTanksIcon.png",
   },
   {
     title: "Battle Boats",
@@ -35,6 +35,6 @@ export const PROJECT_LIST: Project[] = [
     projectType: "C# (Unity)",
     gitHubLink: "https://github.com/NathanWeiland10/BattleBoats-Unity/tree/main",
     playOverWebLink: "https://nathanweiland10.github.io/BattleBoats-WebBuild",
-    projectCover: "pc-icon.png",
+    projectCover: "BattleBoatsIcon.png",
   }
 ]
